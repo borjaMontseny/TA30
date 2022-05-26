b32b6f8deafca286b457e4af016f7bc08c75d72d
Borja Montseny <68342939+borjaMontseny@users.noreply.github.com>
2022-05-26 10:24:40 +0200

    Initial commit

---
+++ Ejercicio01/function.js
@@ -0,0 +1,6 @@
+// Creo una función que dará estos dos mensajes por terminal, no necesita parámetros
+function mensajes() {
+    // el código que se ejecutará
+    console.log("Hello world!");
+    console.log("Este es el primer script");
+  }
---
+++ Ejercicio02/function.js
@@ -0,0 +1,6 @@
+// Creo una función que dará estos dos mensajes por terminal, no necesita parámetros
+function mensaje() {
+    // el código que se ejecutará
+    var saludo = "Hola Mundo! \n Qué fácil es incluir 'comillas simples' \n y ''comillas dobles''";
+    alert(saludo)
+  }
